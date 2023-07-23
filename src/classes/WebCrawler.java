593181eccced05aa5c9a9ae5a62a07e0791881be
Justin Ramirez <ramirez.justin@gmail.com>
2023-07-23 17:19:01 -0400

    added multi memory feature, improved exception handling

--- src/classes/WebCrawler.java
+++ src/classes/WebCrawler.java
@@ -1,94 +1,93 @@
 package classes;
 
-import java.util.ArrayList;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.Connection;
-import java.io.*;
 
-public class WebCrawler implements Runnable{
-	public int MAX_DEPTH;
-	private Thread thread;
-	private String fLink;
-	private ArrayList<String> visitedLinks = new ArrayList<String>();
-	private int ID;
-	public File f = new File("txtFiles/output.txt");
-	public File i = new File("txtFiles/input.txt");
-	
-	//Constructor
-	public WebCrawler(String link, int n, int depth) {
-		System.out.println("New Web Crawler");
-		fLink = link;
-		ID = n;
-		MAX_DEPTH = depth;
-		
-		thread = new Thread(this);
-		thread.start();
-	}
-	
-	@Override
-	public void run() {
-		crawl(1, fLink);
-	}
-	
-	private void crawl(int l, String url) {
-		if(l <= MAX_DEPTH) {
-			Document doc = request(url);
-			
-			if(doc != null) {
-				for(Element links : doc.select("a[href]")) {
-					String next = links.absUrl("href");
-					
-					if(visitedLinks.contains(next) == false && (next.contains("https://") || next.contains("http://"))) {
-						crawl(l++, next);
-					}
-					
-					//Writing out urls to output of crawled pages and remembering most recently crawled page
-					try {
-						PrintWriter flush = new PrintWriter(i);
-						flush.print("");
-						flush.close();
-						PrintWriter out = new PrintWriter(new FileWriter(f, true));
-						PrintWriter in = new PrintWriter(new FileWriter(i, true));
-						out.write(next + "\n");
-						in.write(url + "\n");
-						out.close();
-						in.close();
-					}
-					catch(IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}
-		return;
-	}
-	
-	//Using JSoup to access web pages via http:// or https:// urls
-	private Document request(String url) {
-		try {
-			Connection con = Jsoup.connect(url);
-			Document doc = con.get();
-			
-			if(con.response().statusCode() == 200) {
-				System.out.println(url + " Bot: " + ID);
-				
-				String t = doc.title();
-				System.out.println("Crawling: " + t);
-				visitedLinks.add(url);
-				
-				return doc;
-			}
-			return null;
-		}
-		catch(IOException e) {
-			return null;
-		}
-	}
-	
-	//Returning threads
-	public Thread getThread() {
-		return thread;
-	}
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class WebCrawler implements Runnable {
+    public static final List<String> visitedLinks = Collections.synchronizedList(new ArrayList<>());
+    private static final String OUTPUT_FILE_PATH = "txtFiles/output.txt";
+    private static final String INPUT_FILE_PATH = "txtFiles/input.txt";
+
+    public int MAX_DEPTH;
+    private Thread thread;
+    private String fLink;
+    private int ID;
+
+    //Constructor
+    public WebCrawler(String link, int n, int depth) {
+        System.out.println("New Web Crawler");
+        fLink = link;
+        ID = n;
+        MAX_DEPTH = depth;
+
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        crawl(1, fLink);
+    }
+
+    private void crawl(int level, String url) {
+        if(level <= MAX_DEPTH) {
+            Document doc = request(url);
+
+            if(doc != null) {
+                for(Element links : doc.select("a[href]")) {
+                    String next = links.absUrl("href");
+
+                    if(!visitedLinks.contains(next) && (next.contains("https://") || next.contains("http://"))) {
+                        visitedLinks.add(next);
+                        crawl(level + 1, next);
+                    }
+                }
+
+                //Writing out urls to output of crawled pages and remembering most recently crawled page
+                try(PrintWriter out = new PrintWriter(new FileWriter(OUTPUT_FILE_PATH, true));
+                    PrintWriter in = new PrintWriter(new FileWriter(INPUT_FILE_PATH, true))) {
+
+                    in.println(url);
+                    for(String link : visitedLinks){
+                        out.println(link);
+                    }
+                }
+                catch(IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    //Using JSoup to access web pages via http:// or https:// urls
+    private Document request(String url) {
+        try {
+            Connection con = Jsoup.connect(url);
+            Document doc = con.get();
+
+            if(con.response().statusCode() == 200) {
+                System.out.println(url + " Bot: " + ID);
+
+                String title = doc.title();
+                System.out.println("Crawling: " + title);
+                return doc;
+            }
+            return null;
+        }
+        catch(IOException e) {
+            return null;
+        }
+    }
+
+    //Returning threads
+    public Thread getThread() {
+        return thread;
+    }
 }
--- src/classes/main.java
+++ src/classes/main.java
@@ -4,45 +4,58 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class main {
-	public static void main(String[] args) {
-		@SuppressWarnings("resource")
-		//Getting all needed information from user
-		Scanner console = new Scanner(System.in);
-		System.out.print("Number of threads you'd like to use: ");
-		ArrayList<WebCrawler> bots = new ArrayList<>();
-		int k = 0, n = console.nextInt();
-		System.out.print("Depth for each thread to crawl: ");
-		int d = console.nextInt();
-		
-		//Create WebCrawler bots from list of urls
-		try {
-			BufferedReader r = new BufferedReader(new FileReader("txtFiles/input.txt"));
-			String line;
-			while(k < n && (line = r.readLine()) != null) {
-				bots.add(new WebCrawler(line, k+1, d));
-				k++;
-			}
-			if(k < n) {
-				System.out.println("Not enough Input URLs to create all instances of Web Crawler");
-				System.out.println("Only created " + k + " instances of Web Crawler.");
-			}
-			r.close();
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-		
-		//Start Crawling Threads
-		for(WebCrawler w : bots) {
-			try {
-				w.getThread().join();
-			}
-			catch(InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner console = new Scanner(System.in)) {
+            System.out.print("Number of threads you'd like to use: ");
+            ArrayList<WebCrawler> bots = new ArrayList<>();
+            int k = 0, n = getInt(console);
+            System.out.print("Depth for each thread to crawl: ");
+            int d = getInt(console);
+
+            // Create WebCrawler bots from list of urls
+            try (BufferedReader r = new BufferedReader(new FileReader("txtFiles/input.txt"))) {
+                String line;
+                while (k < n && (line = r.readLine()) != null) {
+                    bots.add(new WebCrawler(line, k + 1, d));
+                    k++;
+                }
+                if (k < n) {
+                    System.out.println("Not enough Input URLs to create all instances of Web Crawler");
+                    System.out.println("Only created " + k + " instances of Web Crawler.");
+                }
+            } catch (IOException e) {
+                System.err.println("Failed to read from the input file. Please make sure the file exists and is readable.");
+                e.printStackTrace();
+            }
+
+            // Start Crawling Threads
+            for (WebCrawler w : bots) {
+                try {
+                    w.getThread().join();
+                } catch (InterruptedException e) {
+                    System.err.println("A thread was interrupted unexpectedly.");
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static ConcurentHashMap<Integer, String> activeUrls = new ConcurentHashMap<>();
+
+    private static int getInt(Scanner scanner) {
+        while (true) {
+            try {
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                scanner.next();
+                System.out.print("Invalid input. Please enter an integer: ");
+            }
+        }
+    }
 }
+
+
